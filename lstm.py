d9ab41416ce861e593841987038bb3cda39dfb5b
Janis Pagel <janis.pagel@uni-koeln.de>
2024-12-12 11:16:33 +0100

    Add exercise

---
+++ lstm.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, Flatten, Input, Dense, Dropout, SimpleRNN, Bidirectional, LSTM
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.regularizers import L1, L2, L1L2
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+df = pd.read_csv("gmb.csv", sep=",")
